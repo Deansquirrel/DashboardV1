caabac83b0255511cca17f3191d090081c6469b3
yuansong6@163.com <yuansong6@163.com>
2020-08-27 17:26:54 +0800

    登录

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,3 +1,5 @@
+import { IResponseMessage } from './message';
+
 export interface ILoginInfo {
   username: string;
   password: string;
@@ -7,4 +9,54 @@ export interface ILoginResponse {
   token: string;
 }
 
-const checkLoing = (username: string, password: string) => {};
+export const checkToken = async (
+  token: string,
+): Promise<IResponseMessage<ILoginResponse>> => {
+  try {
+    const response = fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        token: token,
+      }),
+    });
+    const data = (await response).json();
+    return data;
+  } catch (e) {
+    console.log('request failed', e);
+    return {
+      code: -1,
+      msg: e.message,
+      data: undefined,
+    };
+  }
+};
+
+export const checkLogin = async (
+  username: string,
+  password: string,
+): Promise<IResponseMessage<ILoginResponse>> => {
+  try {
+    const response = fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+    const data = (await response).json();
+    return data;
+  } catch (e) {
+    console.log('request failed', e);
+    return {
+      code: -1,
+      msg: e.message,
+      data: undefined,
+    };
+  }
+};
